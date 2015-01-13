b20e761e5730b41a4171c473c84ccca1c8cdf3c9
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-12 21:51:54 -0800

    added first files

---
+++ R/query.R
@@ -0,0 +1,12 @@
+#' Get documents from a database
+#'
+#' @export
+#' @import httr elastic solr
+#' @importFrom sofa doc_get
+#' @param x asdfafasdf
+#' @examples \dontrun{
+#' doc()
+#' }
+doc <- function(x){
+  "X"
+}
---
+++ R/src.R
@@ -0,0 +1,28 @@
+#' Setup a database connection
+#'
+#' @name src
+#' @param x aasdfasdfsf
+#' @param ... adfafasfasdf
+#' @examples \dontrun{
+#' src_elastic()
+#' src_solr()
+#' src_couchdb()
+#' }
+
+#' @export
+#' @rdname src
+src_elastic <- function(x, ...){
+  "x"
+}
+
+#' @export
+#' @rdname src
+src_solr <- function(x, ...){
+  "x"
+}
+
+#' @export
+#' @rdname src
+src_couchdb <- function(x, ...){
+  "x"
+}
